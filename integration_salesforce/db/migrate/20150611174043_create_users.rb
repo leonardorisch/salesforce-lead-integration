8ce1e70e84ee46a85ec757fd9a87579a4fd15a85
leonardorisch <leo.mrisch@gmail.com>
2015-06-11 17:14:41 -0300

    Try to fix heroku´s deploy

--- integration_salesforce/db/migrate/20150611174043_create_users.rb
+++ integration_salesforce/db/migrate/20150611174043_create_users.rb
@@ -2,13 +2,13 @@ class CreateUsers < ActiveRecord::Migration
   def change
     drop_table :users
     create_table :users do |t|
-      t.string :name
-      t.string :lastname
-      t.string :email
-      t.string :company
-      t.string :jobtitle
-      t.string :phone
-      t.string :website
+      t.string :name, null: false
+      t.string :lastname, null: false
+      t.string :email, null: false
+      t.string :company, null: false
+      t.string :jobtitle, null: false
+      t.string :phone, null: false
+      t.string :website, null: false
 
       t.timestamps null: false
     end
