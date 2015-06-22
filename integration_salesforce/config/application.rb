b26ac1286f5e53e3d0661bf59bbae86f16950e0f
leonardorisch <leo.mrisch@gmail.com>
2015-06-21 22:57:47 -0300

    Version deployed, added some fixes to messages and routes

--- integration_salesforce/config/application.rb
+++ integration_salesforce/config/application.rb
@@ -21,7 +21,7 @@ module IntegrationSalesforce
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
